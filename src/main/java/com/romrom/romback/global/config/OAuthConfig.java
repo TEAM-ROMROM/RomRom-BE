e2bda9ef7d3b1e025202fb2b945200f4a82fa49f
Chuseok22 <bjh59629@naver.com>
2025-02-14 17:23:00 +0900

    소셜 로그인 api 파라미터값 수정 : feat : OAuth2 로그인 방식 변경에 따른 백엔드 OAuth로직 주석처리 https://github.com/TEAM-ROMROM/RomRom-BE/issues/31

--- src/main/java/com/romrom/romback/global/config/OAuthConfig.java
+++ src/main/java/com/romrom/romback/global/config/OAuthConfig.java
@@ -1,16 +1,16 @@
-package com.romrom.romback.global.config;
-
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Component
-@Getter
-public class OAuthConfig {
-
-  @Value("${oauth.kakao.userinfo-url}")
-  private String kakaoUserInfoUrl;
-
-  @Value("${oauth.google.userinfo-url}")
-  private String googleUserInfoUrl;
-}
+//package com.romrom.romback.global.config;
+//
+//import lombok.Getter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Getter
+//public class OAuthConfig {
+//
+//  @Value("${oauth.kakao.userinfo-url}")
+//  private String kakaoUserInfoUrl;
+//
+//  @Value("${oauth.google.userinfo-url}")
+//  private String googleUserInfoUrl;
+//}
