7bb00ed8adbe481b6b7a07034f946753c98bfacb
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-02-28 14:49:04 +0900

    변경 이력관리 팀원 Author 추가 필요 : feat : Author 추가 https://github.com/TEAM-ROMROM/RomRom-BE/issues/43

--- src/main/java/com/romrom/romback/domain/object/constant/Author.java
+++ src/main/java/com/romrom/romback/domain/object/constant/Author.java
@@ -2,16 +2,19 @@ package com.romrom.romback.domain.object.constant;
 
 // Swagger DOCS에서 @ApiChangeLog 어노테이션에 사용
 public enum Author {
-  SUHSAECHAN("서새찬"),
-  BAEKJIHOON("백지훈");
+    SUHSAECHAN("서새찬"),
+    BAEKJIHOON("백지훈"),
+    WISEUNGJAE("위승재"),
+    KIMNAYOUNG("김나영"),
+    KIMKYUSEOP("김규섭");
 
-  private final String displayName;
+    private final String displayName;
 
-  Author(String displayName) {
-    this.displayName = displayName;
-  }
+    Author(String displayName) {
+        this.displayName = displayName;
+    }
 
-  public String getDisplayName() {
-    return displayName;
-  }
+    public String getDisplayName() {
+        return displayName;
+    }
 }
