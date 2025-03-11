87ba9ffed9e5b34e4a9de51ed451d8744a31d571
Chuseok22 <bjh59629@naver.com>
2025-03-11 15:24:01 +0900

    위치인증 데이터 저장 API 구현 : feat : 사용자 위치정보 저장 컨트롤러 구현 https://github.com/TEAM-ROMROM/RomRom-BE/issues/50

--- src/main/java/com/romrom/romback/domain/controller/LocationController.java
+++ src/main/java/com/romrom/romback/domain/controller/LocationController.java
@@ -1,12 +1,11 @@
 package com.romrom.romback.domain.controller;
 
 import com.romrom.romback.domain.object.dto.CustomUserDetails;
-import com.romrom.romback.domain.object.dto.MemberRequest;
-import com.romrom.romback.domain.service.MemberService;
+import com.romrom.romback.domain.object.dto.LocationRequest;
+import com.romrom.romback.domain.service.LocationService;
 import com.romrom.romback.global.aspect.LogMonitoringInvocation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,28 +15,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(
-    name = "회원 API",
-    description = "회원 관련 API 제공"
+    name = "위치 API",
+    description = "위치 관련 API 제공"
 )
 @RestController
-@RequestMapping("/api/members")
 @RequiredArgsConstructor
-public class MemberController implements MemberControllerDocs{
+@RequestMapping("/api/location")
+public class LocationController implements LocationControllerDocs {
 
-  private final MemberService memberService;
+  private final LocationService locationService;
 
-  /**
-   * 회원 선호 카테고리 저장 API
-   * List<Integer> 형식으로 선호 카테고리 코드를 전송합니다.
-   */
-  @Override
-  @PostMapping(value = "/post/category/preferences", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @PostMapping(value = "/post", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   @LogMonitoringInvocation
-  public ResponseEntity<Void> saveMemberProductCategories(
+  @Override
+  public ResponseEntity<Void> saveLocation(
       @AuthenticationPrincipal CustomUserDetails customUserDetails,
-      @ModelAttribute MemberRequest request) {
+      @ModelAttribute LocationRequest request) {
     request.setMember(customUserDetails.getMember());
-    memberService.saveMemberProductCategories(request);
-    return ResponseEntity.status(HttpStatus.CREATED).build();
+    locationService.saveLocation(request);
+    return ResponseEntity.ok().build();
   }
 }
---
+++ src/main/java/com/romrom/romback/domain/controller/LocationControllerDocs.java
@@ -0,0 +1,49 @@
+package com.romrom.romback.domain.controller;
+
+import com.romrom.romback.domain.object.constant.Author;
+import com.romrom.romback.domain.object.dto.CustomUserDetails;
+import com.romrom.romback.domain.object.dto.LocationRequest;
+import com.romrom.romback.global.docs.ApiChangeLog;
+import com.romrom.romback.global.docs.ApiChangeLogs;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+public interface LocationControllerDocs {
+
+  @ApiChangeLogs({
+      @ApiChangeLog(
+          date = "2025.03.11",
+          author = Author.BAEKJIHOON,
+          issueNumber = 50,
+          description = "사용자 위치인증 데이터 저장"
+      )
+  })
+  @Operation(
+      summary = "사용자 위치 데이터 저장",
+      description = """
+      ## 인증(JWT): **필요**
+      
+      ## 요청 파라미터 (MemberRequest)
+      - **`longitude`**: 경도 (double)
+      - **`latitude`**: 위도도 (double)
+      - **`siDo`**: 시/도 (String)
+      - **`siGunGu`**: 시/군/구 (String)
+      - **`eupMyoenDong`**: 읍/면/동 (String)
+      - **`ri`**: 리 (String)
+      - **`fullAddress`**: 지번 주소 (String)
+      - **`roadAddress`**: 도로명 주소 (String)
+      
+      ## 반환값
+      - LocationResponse
+      
+      ## 에러코드
+      - **`INVALID_MEMBER`**: 유효하지 않은 회원 정보입니다.
+      - **`INVALID_CATEGORY_CODE`**: 존재하지 않는 카테고리 코드입니다.
+    """
+  )
+  ResponseEntity<Void> saveLocation(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails,
+      @ModelAttribute LocationRequest request);
+}
