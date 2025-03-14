41ba76b2a1f6c7198eee1fff0fb2187db16b5e8d
Kim na young <kimna041208@gmail.com>
2025-03-15 02:10:50 +0900

    상품 등록 CRUD : feat : 상품 등록 서비스 및 컨트롤러 구현 https://github.com/TEAM-ROMROM/RomRom-BE/issues/55

---
+++ src/main/java/com/romrom/romback/domain/object/dto/ItemRequest.java
@@ -0,0 +1,45 @@
+package com.romrom.romback.domain.object.dto;
+
+import com.romrom.romback.domain.object.constant.ItemCategory;
+import com.romrom.romback.domain.object.constant.ItemCondition;
+import com.romrom.romback.domain.object.constant.TradeOption;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+@ToString
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class ItemRequest {
+
+  @Schema(description = "상품 사진")
+  private List<MultipartFile> itemImages = new ArrayList<>();
+
+  @Schema(description = "상품명")
+  private String itemName;
+
+  @Schema(description = "상품 상세 설명")
+  private String itemDescription;
+
+  @Schema(description = "상품 카테고리")
+  private ItemCategory itemCategory;
+
+  @Schema(description = "상품 상태")
+  private ItemCondition itemCondition;
+
+  @Schema(description = "상품 옵션 (추가금, 직거래만, 택배거래만)")
+  private List<TradeOption> tradeOptions = new ArrayList<>();
+
+  @Schema(description = "가격")
+  private Integer price;
+}
