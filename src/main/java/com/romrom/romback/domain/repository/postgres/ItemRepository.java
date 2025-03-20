d9bcae13450cc2ca5f5cd3b14461fa0ec8afabd3
Kim na young <kimna041208@gmail.com>
2025-03-20 22:49:08 +0900

    첫 물품 등록 여부 확인 : fix: ItemService 에서 첫 물품 등록 시 업데이트로 변경 https://github.com/TEAM-ROMROM/RomRom-BE/issues/49

--- src/main/java/com/romrom/romback/domain/repository/postgres/ItemRepository.java
+++ src/main/java/com/romrom/romback/domain/repository/postgres/ItemRepository.java
@@ -5,6 +5,4 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, UUID> {
-
-  Boolean existsByMember_MemberId(UUID memberId);
 }
