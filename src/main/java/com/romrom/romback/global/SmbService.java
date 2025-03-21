6852d54691d8a19b14697a7834f2a6563f7bd810
Chuseok22 <bjh59629@naver.com>
2025-03-21 14:16:05 +0900

    Revert "파일 전송 프로토콜 : feat : SmbService 클래스 uploadFile 메소드 반환값 FileResponse 추가 https://github.com/TEAM-ROMROM/RomRom-BE/issues/51"
    This reverts commit 66bfb501e82766b6843307dfb66168765f22d42e.

--- src/main/java/com/romrom/romback/global/SmbService.java
+++ src/main/java/com/romrom/romback/global/SmbService.java
@@ -3,7 +3,6 @@ package com.romrom.romback.global;
 import static com.romrom.romback.global.util.CommonUtil.nvl;
 
 import com.romrom.romback.domain.object.constant.MimeType;
-import com.romrom.romback.domain.object.dto.FileResponse;
 import com.romrom.romback.global.exception.CustomException;
 import com.romrom.romback.global.exception.ErrorCode;
 import com.romrom.romback.global.util.FileUtil;
@@ -11,7 +10,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -46,7 +44,7 @@ public class SmbService {
    * SMB 파일 업로드 (다중)
    */
   @Transactional
-  public CompletableFuture<List<FileResponse>> uploadFile(List<MultipartFile> files) {
+  public CompletableFuture<List<String>> uploadFile(List<MultipartFile> files) {
 
     // 1. 파일 업로드 개수 확인
     if (files.size() > UPLOAD_FILE_MAX_COUNT || files.isEmpty()) {
@@ -56,10 +54,10 @@ public class SmbService {
     int requestFileCount = files.size();
 
     // 2. 멀티스레드 파일 저장
-    List<CompletableFuture<FileResponse>> futures = new ArrayList<>();
+    List<CompletableFuture<String>> futures = new ArrayList<>();
 
     for (MultipartFile file : files) {
-      CompletableFuture<FileResponse> future = CompletableFuture.supplyAsync(() -> {
+      CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
         try {
           return uploadFile(file);
         } catch (Exception e) {
@@ -73,12 +71,12 @@ public class SmbService {
     // 3. 모든 비동기 작업 결과 확인
     return CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new))
         .thenApply(result -> {
-          List<FileResponse> successfulUploadResponses = new ArrayList<>();
+          List<String> successfulUploads = new ArrayList<>();
           boolean failureOccurred = false;
 
-          for (CompletableFuture<FileResponse> future : futures) {
+          for (CompletableFuture<String> future : futures) {
             try {
-              successfulUploadResponses.add(future.get());
+              successfulUploads.add(future.get());
             } catch (Exception e) {
               log.error("비동기 파일 업로드 실패: {}", e.getMessage());
               failureOccurred = true;
@@ -86,17 +84,14 @@ public class SmbService {
           }
 
           // 실패가 발생했거나, 성공한 파일 수가 요청 파일 수와 다른 경우
-          if (failureOccurred || successfulUploadResponses.size() != requestFileCount) {
-            List<String> successfulUploadsFileNames = successfulUploadResponses.stream()
-                .map(FileResponse::getUploadedFileName)
-                .collect(Collectors.toList());
-            log.error("파일 업로드 실패: 실패 개수={}", requestFileCount - successfulUploadsFileNames.size());
-            log.debug("업로드한 파일 롤백 시작: 업로드 된 파일 개수={}", successfulUploadsFileNames.size());
-            deleteFile(successfulUploadsFileNames);
+          if (failureOccurred || successfulUploads.size() != requestFileCount) {
+            log.error("파일 업로드 실패: 실패 개수={}", requestFileCount - successfulUploads.size());
+            log.debug("업로드한 파일 롤백 시작: 업로드 된 파일 개수={}", successfulUploads.size());
+            deleteFile(successfulUploads);
             throw new CustomException(ErrorCode.FILE_UPLOAD_ERROR);
           }
-          log.debug("SMB 파일 업로드 완료: {}개 성공", successfulUploadResponses.size());
-          return successfulUploadResponses;
+          log.debug("SMB 파일 업로드 완료: {}개 성공", successfulUploads.size());
+          return successfulUploads;
         })
         .exceptionally(throwable -> {
           log.error("파일 업로드 중 전체 오류 발생: {}", throwable.getMessage());
@@ -106,46 +101,42 @@ public class SmbService {
 
   /**
    * SMB 파일 업로드 (단일)
+   *
+   * @return 업로드 된 파일 Path
    */
   @Transactional
-  public FileResponse uploadFile(MultipartFile file) {
+  public String uploadFile(MultipartFile file) {
 
-    // 1. 파일 유효성 검사
-    validateFile(file);
+    try {
+      // 1. 파일 유효성 검사
+      validateFile(file);
 
-    // 2. 파일 이름 설정
-    String fileName = FileUtil.generateFilename(file.getOriginalFilename());
-    String filePath = FileUtil.generateSmbFilePath(rootDir, dir, fileName);
+      // 2. 파일 이름 설정
+      String fileName = FileUtil.generateFilename(file.getOriginalFilename());
+      String filePath = FileUtil.generateSmbFilePath(rootDir, dir, fileName);
 
-    // 3. InputStream 생성
-    try (InputStream inputStream = file.getInputStream()) {
-      // 4. 메시지 생성 및 헤더 설정
-      Message<InputStream> message = MessageBuilder
-          .withPayload(inputStream)
-          .setHeader("file_name", fileName)
-          .build();
-      log.debug("SMB 메시지 생성 완료: {}", message);
+      // 3. InputStream 생성
+      try (InputStream inputStream = file.getInputStream()) {
+        // 4. 메시지 생성 및 헤더 설정
+        Message<InputStream> message = MessageBuilder
+            .withPayload(inputStream)
+            .setHeader("file_name", fileName)
+            .build();
+        log.debug("SMB 메시지 생성 완료: {}", message);
 
-      // 5. 파일 업로드
-      log.debug("SMB 파일 업로드 시작: 파일명={}, 크기={} 바이트", fileName, file.getSize());
-      smbUploadChannel.send(message);
-      log.debug("SMB 파일 업로드 성공: {}", fileName);
+        // 5. 파일 업로드
+        log.debug("SMB 파일 업로드 시작: 파일명={}, 크기={} 바이트", fileName, file.getSize());
+        smbUploadChannel.send(message);
+        log.debug("SMB 파일 업로드 성공: {}", fileName);
 
-      return FileResponse.builder()
-          .originalFileName(file.getOriginalFilename())
-          .uploadedFileName(fileName)
-          .filePath(filePath)
-          .fileSize(file.getSize())
-          .build();
+        return filePath;
+      }
     } catch (Exception e) {
       log.error("SMB 파일 업로드 실패: 파일명={}", file.getOriginalFilename(), e);
       throw new CustomException(ErrorCode.FILE_UPLOAD_ERROR);
     }
   }
 
-  /**
-   * SMB 파일 삭제
-   */
   public void deleteFile(List<String> fileNames) {
 
     log.debug("SMB 파일 삭제 시작: 요청된 파일 개수={}", fileNames.size());
