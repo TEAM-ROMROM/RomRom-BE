685c7ae7440195f4ef199d48cbc41cad407a894c
Chuseok22 <bjh59629@naver.com>
2025-03-27 10:32:56 +0900

    어플 등록을 위한 회원 탈퇴 로직 구현 : feat : 각 repository에 엔티티 삭제를 위한 delete 메서드 추가 https://github.com/TEAM-ROMROM/RomRom-BE/issues/69

--- src/main/java/com/romrom/romback/domain/repository/postgres/MemberProductCategoryRepository.java
+++ src/main/java/com/romrom/romback/domain/repository/postgres/MemberProductCategoryRepository.java
@@ -1,14 +1,13 @@
 package com.romrom.romback.domain.repository.postgres;
 
-import com.romrom.romback.domain.object.postgres.Member;
 import com.romrom.romback.domain.object.postgres.MemberItemCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberProductCategoryRepository extends JpaRepository<MemberItemCategory, UUID> {
 
   List<MemberItemCategory> findByMemberMemberId(UUID memberId);
 
-  void deleteByMember(Member member);
+  void deleteByMemberMemberId(UUID memberId);
 }
