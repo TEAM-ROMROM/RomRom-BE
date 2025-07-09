c1d35de040f728456eadb6ecd7d3e5d2bd48d3c2
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-07-09 21:31:27 +0900

    물품 신고하기 API 개발 : feat : 신고 API 설계 및 엔드포인트 정의

---
+++ RomRom-Domain-Report/src/main/java/com/romrom/report/dto/ReportRequest.java
@@ -0,0 +1,34 @@
+package com.romrom.report.dto;
+
+import com.romrom.member.entity.Member;
+import com.romrom.report.enums.ReportReason;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.Set;
+import java.util.UUID;
+
+@ToString
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ReportRequest {
+
+  @Schema(description = "회원")
+  private Member member;
+
+  @Schema(description = "물품 ID")
+  private UUID itemId;
+
+  @Schema(description = "신고 사유 코드들", required = true,
+      example = "[\"FRAUD\", \"INAPPROPRIATE\"]")
+  @NotEmpty
+  private Set<ReportReason> reasons;
+
+  @Size(max = 300)
+  @Schema(description = "기타 입력 내용")
+  private String extraComment;
+}
---
+++ RomRom-Web/src/main/java/com/romrom/web/controller/ReportController.java
@@ -0,0 +1,34 @@
+package com.romrom.web.controller;
+
+
+import com.romrom.auth.dto.CustomUserDetails;
+import com.romrom.report.dto.ReportRequest;
+import com.romrom.report.entity.Report;
+import com.romrom.report.service.ReportService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(
+    name = "신고 관련 API",
+    description = "신고(아이템 등) 관련 API 제공"
+)
+@RequestMapping("/api/report")
+public class ReportController implements ReportControllerDocs {
+
+  private final ReportService reportService;
+
+  @PostMapping("/{itemId}/reports")
+  public ResponseEntity<Void> reportItem(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                      @ModelAttribute ReportRequest request) {
+    request.setMember(customUserDetails.getMember());
+    reportService.createReport(request);
+    return ResponseEntity.ok().build();
+  }
+
+
+}
