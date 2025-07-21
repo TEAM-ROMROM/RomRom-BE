5b15ec52eb1bcbb5533c2797376c4c3747df64e4
nayoungKim <kimna041208@gmail.com>
2025-07-22 03:04:00 +0900

    firebase 알림 서비스 구현 : feat : FCM 토큰 등록 로직 구현 https://github.com/TEAM-ROMROM/RomRom-BE/issues/228

---
+++ RomRom-Common/src/main/java/com/romrom/common/constant/NotificationConstants.java
@@ -0,0 +1,10 @@
+package com.romrom.common.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class NotificationConstants {
+
+  // FCM Token
+  public static final long FCM_TOKEN_TTL = 30 * 24 * 60 * 60L; // FCM 토큰 30일 후 파기
+}
---
+++ RomRom-Domain-Notification/src/main/java/com/romrom/notification/service/FcmTokenService.java
@@ -0,0 +1,56 @@
+package com.romrom.notification.service;
+
+import com.romrom.notification.dto.NotificationRequest;
+import com.romrom.notification.dto.NotificationResponse;
+import com.romrom.notification.entity.FcmToken;
+import com.romrom.notification.repository.FcmTokenRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class FcmTokenService {
+
+  private final FcmTokenRepository fcmTokenRepository;
+
+  @Transactional
+  public NotificationResponse saveFcmToken(NotificationRequest request) {
+    // DTO → Entity
+    FcmToken fcmToken = FcmToken.builder()
+        .token(request.getFcmToken())
+        .memberId(request.getMember().getMemberId())
+        .deviceType(request.getDeviceType())
+        .build();
+
+    // Redis 저장 (동일 키 시 덮어쓰기)
+    fcmTokenRepository.save(fcmToken);
+
+    log.debug("토큰 저장 완료");
+    log.debug("사용자 ID : {}", fcmToken.getMemberId());
+    log.debug("사용자 기기 : {}", fcmToken.getDeviceType());
+
+    // Entity → Response DTO
+    return new NotificationResponse(
+        fcmToken.getFcmTokenId(),
+        fcmToken.getToken(),
+        fcmToken.getMemberId(),
+        fcmToken.getDeviceType()
+    );
+  }
+
+  public List<FcmToken> findAllTokensByMemberId(UUID memberId) {
+    return fcmTokenRepository.findAllByMemberId(memberId);
+  }
+
+  public List<FcmToken> findAllTokens() {
+    List<FcmToken> fcmTokenList = new ArrayList<>();
+    fcmTokenRepository.findAll().forEach(fcmTokenList::add);
+    return fcmTokenList;
+  }
+}
