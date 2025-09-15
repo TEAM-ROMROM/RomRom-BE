4625dfcf8865d4a4cbf5b333a1047d2aa178fb89
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-09-15 23:29:46 +0900

    물품관리에 대한 API 연결 및 페이지네이션 구현 필요 : fix : 로그인 오류 구현 및 Page 직렬화 경고 제거 안전파싱으로 변경 https://github.com/TEAM-ROMROM/RomRom-BE/issues/328

--- RomRom-Web/src/main/java/com/romrom/web/RomBackApplication.java
+++ RomRom-Web/src/main/java/com/romrom/web/RomBackApplication.java
@@ -2,8 +2,10 @@ package com.romrom.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class RomBackApplication {
 
     public static void main(String[] args) {
