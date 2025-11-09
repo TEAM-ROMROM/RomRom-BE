6776a88d271ac3c83b9a95162116a8fa48f74ff3
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-11-09 15:34:22 +0900

    채팅방별 읽지 않은 메시지 개수 제공 : feat : 멤버위치 조회 쿼리에 @Query 추가 #318

--- RomRom-Domain-Member/src/main/java/com/romrom/member/repository/MemberLocationRepository.java
+++ RomRom-Domain-Member/src/main/java/com/romrom/member/repository/MemberLocationRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberLocationRepository extends JpaRepository<MemberLocation, UUID> {
   Optional<MemberLocation> findByMemberMemberId(UUID memberId);
@@ -18,5 +19,6 @@ public interface MemberLocationRepository extends JpaRepository<MemberLocation, 
    * MemberLocation이 Member를 FetchType.LAZY로 가지고 있으므로,
    * Member ID를 직접 조회하도록 @Query를 사용하거나 메서드 이름을 사용합니다.
    */
+  @Query("SELECT ml FROM MemberLocation ml WHERE ml.member.memberId IN :memberIds")
   List<MemberLocation> findByMemberMemberIdIn(Set<UUID> memberIds);
 }
