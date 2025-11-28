f668799dc707bd65341766bbe171b83e5ebd6fdb
Chuseok22 <bjh59629@naver.com>
2025-11-28 15:53:33 +0900

    회원정보_수정_화면_접근시_회원이_받은_총_좋아요수_개수_반환_필요 : feat : flyway migration 테이블 유/무 방어로직 추가 https://github.com/TEAM-ROMROM/RomRom-BE/issues/411

--- RomRom-Web/src/main/resources/db/migration/V1_1_19__add_member_total_like_count.sql
+++ RomRom-Web/src/main/resources/db/migration/V1_1_19__add_member_total_like_count.sql
@@ -1,16 +1,27 @@
 -- Member 테이블에 total_like_count 컬럼 추가
-ALTER TABLE member
-  ADD COLUMN total_like_count integer NOT NULL DEFAULT 0;
+ALTER TABLE IF EXISTS member
+  ADD COLUMN IF NOT EXISTS total_like_count integer NOT NULL DEFAULT 0;
 
 -- 기존 Item.like_count 를 기준으로 Member.total_like_count 동기화
-UPDATE member m
-SET total_like_count = COALESCE(item_like_sum.total_like_count, 0)
-FROM (
-       SELECT i.member_member_id,
-              COALESCE(SUM(i.like_count), 0) AS total_like_count
-       FROM item i
-       WHERE i.is_deleted = FALSE
-       GROUP BY i.member_member_id
+DO
+$$
+  BEGIN
+    IF TO_REGCLASS('public_member') IS NOT NULL
+      AND TO_REGCLASS('public.item') IS NOT NULL THEN
 
-     ) AS item_like_sum
-WHERE m.member_id = item_like_sum.member_member_id;
+      UPDATE member m
+      SET total_like_count = COALESCE(item_like_sum.total_like_count, 0)
+      FROM (
+             SELECT i.member_member_id,
+                    COALESCE(SUM(i.like_count), 0) AS total_like_count
+             FROM item i
+             WHERE i.is_deleted = FALSE
+             GROUP BY i.member_member_id
+
+           ) AS item_like_sum
+      WHERE m.member_id = item_like_sum.member_member_id;
+
+    END IF;
+  END;
+
+$$;
