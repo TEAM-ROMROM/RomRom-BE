318256a630cd837b602958aa3b92746d401e07b3
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2026-01-04 19:30:44 +0900

    회원차단 기능 개발 : feat : 채팅방 조회 시 상대방 차단 여부 boolean 추가 #428

--- RomRom-Domain-Member/src/main/java/com/romrom/member/repository/MemberBlockRepository.java
+++ RomRom-Domain-Member/src/main/java/com/romrom/member/repository/MemberBlockRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,4 +19,9 @@ public interface MemberBlockRepository extends JpaRepository<MemberBlock, UUID> 
   @Modifying
   @Query("DELETE FROM MemberBlock b WHERE b.blockerMember.memberId = :blockerId AND b.blockedMember.memberId = :blockedId")
   void deleteByBlockerIdAndBlockedId(@Param("blockerId") UUID blockerId, @Param("blockedId") UUID blockedId);
+
+  @Query("SELECT mb FROM MemberBlock mb " +
+      "WHERE (mb.blockerMember.memberId = :myId AND mb.blockedMember.memberId IN :targetIds) " +
+      "   OR (mb.blockerMember.memberId IN :targetIds AND mb.blockedMember.memberId = :myId)")
+  List<MemberBlock> findAllBlockRelations(@Param("myId") UUID myId, @Param("targetIds") Collection<UUID> targetIds);
 }
