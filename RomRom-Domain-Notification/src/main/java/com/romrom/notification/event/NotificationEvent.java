0d78a64895ba19bd45120f72b52773cafdd1e03d
Chuseok22 <bjh59629@naver.com>
2026-01-28 10:08:43 +0900

    기획_플로우별_Firebase_FCM_푸시_알림_발송 : feat : 알림 payload 추가 https://github.com/TEAM-ROMROM/RomRom-BE/issues/441

--- RomRom-Domain-Notification/src/main/java/com/romrom/notification/event/NotificationEvent.java
+++ RomRom-Domain-Notification/src/main/java/com/romrom/notification/event/NotificationEvent.java
@@ -1,6 +1,9 @@
 package com.romrom.notification.event;
 
+import com.romrom.common.util.CommonUtil;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import lombok.Getter;
 
@@ -10,19 +13,35 @@ public abstract class NotificationEvent {
   private final UUID targetMemberId;
   private final NotificationType notificationType;
   private final LocalDateTime publishedAt;
-  private String deepLink;
+  private final Map<String, String> payload;
 
   protected NotificationEvent(UUID targetMemberId, NotificationType notificationType) {
     this.targetMemberId = targetMemberId;
     this.notificationType = notificationType;
     this.publishedAt = LocalDateTime.now();
+    this.payload = new HashMap<>();
+
+    this.payload.put("notificationType", notificationType.name());
+    this.payload.put("publishedAt", String.valueOf(publishedAt));
   }
 
   public abstract String getTitle();
 
   public abstract String getBody();
 
+  protected void addPayload(String key, String value) {
+    if (!CommonUtil.nvl(value, "").isEmpty()) {
+      this.payload.put(key, value);
+    }
+  }
+
+  protected void addPayload(String key, UUID value) {
+    if (value != null) {
+      this.payload.put(key, value.toString());
+    }
+  }
+
   protected void setDeepLink(String deepLink) {
-    this.deepLink = deepLink;
+    this.payload.put("deepLink", deepLink);
   }
 }
