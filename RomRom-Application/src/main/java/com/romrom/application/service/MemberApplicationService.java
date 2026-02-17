628ae79aa2d889dd6c61e4a0ad8c0fca75029aab
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2026-02-17 10:49:52 +0900

    채팅방_나가기_안되는_버그 : feat : 몽고 트랜잭션 매니저 관련 코드 제거 #521

--- RomRom-Application/src/main/java/com/romrom/application/service/MemberApplicationService.java
+++ RomRom-Application/src/main/java/com/romrom/application/service/MemberApplicationService.java
@@ -35,7 +35,7 @@ public class MemberApplicationService {
      *
      * @param request member
      */
-    @Transactional(transactionManager = "chainedTransactionManager")
+    @Transactional
     public void deleteMember(MemberRequest request, HttpServletRequest httpServletRequest) {
         Member member = request.getMember();
         UUID memberId = member.getMemberId();
